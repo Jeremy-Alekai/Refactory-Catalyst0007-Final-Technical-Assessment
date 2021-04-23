1aa527593018c63d1a0c74bb6fd2347ed3199b83
Jeremy Alekai <57034956+Jeremy-Alekai@users.noreply.github.com>
2021-04-23 14:24:56 +0300

    Jeremy Techincal Assessment

---
+++ Anecho Jeremy Stewart/index.js
@@ -0,0 +1,50 @@
+//Dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+
+//Requiring a route
+const covidRoute = require('./routes/covidRoute');
+
+require('dotenv').config();
+
+// Instatiate express with app
+const app = express()
+
+//Middleware handling data from the form
+app.use(express.urlencoded({
+    extended: true
+}));
+
+//middleware for serving static files
+app.use(express.static('public'));
+
+
+//Middleware configurations 
+app.set('views', 'views');
+app.set('view engine', 'pug');
+
+
+//Functions as handlers for Routes 
+app.use('/', covidRoute);
+
+//Db connection configurations.
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+//Post connection to the database
+mongoose.connection
+    .on('open', () => {
+        console.log('Mongoose connection open');
+    })
+    .on('error', (err) => {
+        console.log(`Connection error: ${err.message}`);
+    });
+
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+})
